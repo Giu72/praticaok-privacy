60952d7b83bf17d1135a960c79111f7441761c90
Giu72 <giusepperomeo029@gmail.com>
2026-05-10 12:01:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 const express = require('express');
-const app = express();
-app.use(express.json());
+const bodyParser = require('body-parser');
+const app = express().use(bodyParser.json());
 
+// LA TUA PAROLA D'ORDINE
+const token = "progetto2024"; 
+
+// QUESTO SERVE PER "VERIFICA E SALVA"
 app.get('/webhook', (req, res) => {
-  const verify_token = "progetto2024"; // Scegli tu una parola
-  if (req.query['hub.verify_token'] === verify_token) {
-    res.send(req.query['hub.challenge']);
-  } else {
-    res.sendStatus(403);
-  }
+    let mode = req.query['hub.mode'];
+    let challenge = req.query['hub.challenge'];
+    let verify_token = req.query['hub.verify_token'];
+
+    if (mode && verify_token) {
+        if (mode === 'subscribe' && verify_token === token) {
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+        } else {
+            res.sendStatus(403);
+        }
+    }
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('Webhook in ascolto'));
+// QUESTO SERVE PER RICEVERE I MESSAGGI
+app.post('/webhook', (req, res) => {
+    console.log('Messaggio ricevuto:', JSON.stringify(req.body, null, 2));
+    res.sendStatus(200);
+});
+
+app.listen(process.env.PORT || 10000, () => console.log('Server in ascolto...'));
